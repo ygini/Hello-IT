cb77064774124424e0497e850e44d98942cb1fef
Yoann Gini <yoann.gini@gmail.com>
2017-09-25 10:15:09 +0200

    Initial version of AD Pass plugin

---
+++ src/Plugins/ADPass/ADPass/HITPADPass.m
@@ -0,0 +1,13 @@
+//
+//  HITPADPass.m
+//  ADPass
+//
+//  Created by Yoann Gini on 24/09/2017.
+//  Copyright © 2017 Yoann Gini. All rights reserved.
+//
+
+#import "HITPADPass.h"
+
+@implementation HITPADPass
+
+@end
