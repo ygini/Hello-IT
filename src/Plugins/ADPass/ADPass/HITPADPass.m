4c2f7328223f3a5f66ff0ab09f2e35a7e8f3b672
Yoann Gini <yoann.gini@gmail.com>
2017-09-25 11:49:24 +0200

    Alert user to change AD password via notif, one per day

--- src/Plugins/ADPass/ADPass/HITPADPass.m
+++ src/Plugins/ADPass/ADPass/HITPADPass.m
@@ -11,25 +11,34 @@
 
 #define kHITPADPassExpiryTimeField @"msDS-UserPasswordExpiryTimeComputed"
 #define kHITPADPassExpiryTimeKey @"public.ad.pass.expirySince1970"
+#define kHITPADPassLastNotifKey @"public.ad.pass.lastNotification"
 
 #define kHITPADPassWillExpireFormat @"willExpireFormat"
 #define kHITPADPassTooltip @"tooltip"
-#define kHITPADPassDisabledTooltip @"disabledTooltip"
+#define kHITPADPassOfflineTooltip @"offlineTooltip"
+
+#define kHITPADPassNotifTitle @"notificationTitle"
+#define kHITPADPassNotifMessageFormat @"notificationMessageFormat"
+#define kHITPADPassNotifOfflineMessageFormat @"notificationOfflineMessageFormat"
+
+#define kHITPADPassAlertXDaysBefore @"alertXDaysBefore"
 
 @interface HITPADPass ()
 
-@property NSDate *passwordExpiracyDate;
+@property NSDate *passwordExpiryDate;
 @property BOOL lastADRequestSucceded;
 
 @property NSString *localeTablePath;
 
-@property NSString *defaultWillExpireFormat;
-@property NSString *defaultTooltip;
-@property NSString *defaultDisabledTooltip;
-
 @property NSString *willExpireFormat;
 @property NSString *tooltip;
-@property NSString *disabledTooltip;
+@property NSString *offlineTooltip;
+
+@property NSString *notificationTitle;
+@property NSString *notificationMessageFormat;
+@property NSString *notificationOfflineMessageFormat;
+
+@property NSInteger alertXDaysBefore;
 
 @end
 
@@ -39,29 +48,55 @@
 {
     self = [super initWithSettings:settings];
     if (self) {
-        _defaultWillExpireFormat = @"🔐 📆 %ld";
-        _defaultTooltip = @"Delay before your password expire, change it before!";
-        _defaultDisabledTooltip = @"Your IT infrastructure isn't available. Current info is based on the last recorded value.";
+        _alertXDaysBefore = [[settings objectForKey:kHITPADPassAlertXDaysBefore] integerValue];
+        
+        if (_alertXDaysBefore == 0) {
+            _alertXDaysBefore = 15;
+        }
+        
+        NSString *defaultWillExpireFormat = @"🔐 📆 %ld";
+        NSString *defaultTooltip = @"Delay before your password expire, change it before!";
+        NSString *defaultOfflineTooltip = @"Your IT infrastructure isn't available. Current info is based on the last recorded value.";
         
         _willExpireFormat = [settings objectForKey:kHITPADPassWillExpireFormat];
         _tooltip = [settings objectForKey:kHITPADPassTooltip];
-        _disabledTooltip = [settings objectForKey:kHITPADPassDisabledTooltip];
+        _offlineTooltip = [settings objectForKey:kHITPADPassOfflineTooltip];
         
         if ([_willExpireFormat length] == 0) {
-            _willExpireFormat = _defaultWillExpireFormat;
+            _willExpireFormat = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassWillExpireFormat value:defaultWillExpireFormat table:nil];
         }
         
         if ([_tooltip length] == 0) {
-            _tooltip = _defaultTooltip;
+            _tooltip = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassTooltip value:defaultTooltip table:nil];
         }
         
-        if ([_disabledTooltip length] == 0) {
-            _disabledTooltip = _defaultDisabledTooltip;
+        if ([_offlineTooltip length] == 0) {
+            _offlineTooltip = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassOfflineTooltip value:defaultOfflineTooltip table:nil];
         }
         
-        _willExpireFormat = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassWillExpireFormat value:_willExpireFormat table:nil];
-        _tooltip = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassTooltip value:_tooltip table:nil];
-        _disabledTooltip = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassDisabledTooltip value:_disabledTooltip table:nil];
+        
+        
+        NSString *defaultNotificationTitle = @"🔐 📆 ⚠️";
+        NSString *defaultNotificationMessageFormat = @"Your password will expire on %@. Change it before!";
+        NSString *defaultNotificationOfflineMessageFormat = @"Your password will expire on %@. Come back on your corporate network and change it before!";
+        
+        _notificationTitle = [settings objectForKey:kHITPADPassNotifTitle];
+        _notificationMessageFormat = [settings objectForKey:kHITPADPassNotifMessageFormat];
+        _notificationOfflineMessageFormat = [settings objectForKey:kHITPADPassNotifOfflineMessageFormat];
+        
+        if ([_notificationTitle length] == 0) {
+            _notificationTitle = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassNotifTitle value:defaultNotificationTitle table:nil];
+        }
+        
+        if ([_notificationMessageFormat length] == 0) {
+            _notificationMessageFormat = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassNotifMessageFormat value:defaultNotificationMessageFormat table:nil];
+        }
+        
+        if ([_notificationOfflineMessageFormat length] == 0) {
+            _notificationOfflineMessageFormat = [[NSBundle bundleForClass:[self class]] localizedStringForKey:kHITPADPassNotifOfflineMessageFormat value:defaultNotificationOfflineMessageFormat table:nil];
+        }
+
+
     }
     return self;
 }
@@ -77,12 +112,37 @@
 
 - (void)updateTitle {
     
-    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate:[NSDate date] toDate:self.passwordExpiracyDate options:0];
-    
-    self.menuItem.title = [NSString stringWithFormat:self.willExpireFormat, (long)[components day]];
+    NSDateComponents *components = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate:[NSDate date] toDate:self.passwordExpiryDate options:0];
+    long daysBeforeExpiry = (long)[components day];
+    self.menuItem.title = [NSString stringWithFormat:self.willExpireFormat, daysBeforeExpiry];
     self.menuItem.enabled = self.lastADRequestSucceded;
     
-    self.menuItem.toolTip = self.lastADRequestSucceded ? self.tooltip : self.disabledTooltip;
+    self.menuItem.toolTip = self.lastADRequestSucceded ? self.tooltip : self.offlineTooltip;
+    
+    if (daysBeforeExpiry <= self.alertXDaysBefore) {
+        NSDate *dateOfLastNotification = [[NSUserDefaults standardUserDefaults] objectForKey:kHITPADPassLastNotifKey];
+        
+        BOOL notifNeeded = NO;
+        
+        if (dateOfLastNotification) {
+            NSDateComponents *previousNotifComponents = [[NSCalendar currentCalendar] components:NSCalendarUnitDay fromDate:dateOfLastNotification toDate:self.passwordExpiryDate options:0];
+            notifNeeded = [previousNotifComponents day] != daysBeforeExpiry;
+        } else {
+            notifNeeded = YES;
+        }
+        
+        if (notifNeeded) {
+            NSDateComponents* components = [[NSCalendar  currentCalendar] components:NSCalendarUnitYear | NSCalendarUnitMonth | NSCalendarUnitDay | NSCalendarUnitHour | NSCalendarUnitMinute fromDate:[NSDate date]];
+            
+            components.hour = 10;
+            components.minute = 00;
+            
+            NSDate* deliveryDate = [[NSCalendar  currentCalendar] dateFromComponents:components];
+            
+            [self sendUserNotificationWithDeliveryDate:deliveryDate];
+            [[NSUserDefaults standardUserDefaults] setObject:[NSDate date] forKey:kHITPADPassLastNotifKey];
+        }
+    }
 }
 
 - (void)getPasswordExpiryDate {
@@ -126,15 +186,32 @@
         NSTimeInterval expiryTimeInterval = [expiryTime integerValue] / 10000000.0;
 
         
-        self.passwordExpiracyDate = [NSDate dateWithTimeInterval:expiryTimeInterval sinceDate:adRefenreceDate];
+        self.passwordExpiryDate = [NSDate dateWithTimeInterval:expiryTimeInterval sinceDate:adRefenreceDate];
         self.lastADRequestSucceded = YES;
         
-        [[NSUserDefaults standardUserDefaults] setInteger:[self.passwordExpiracyDate timeIntervalSince1970] forKey:kHITPADPassExpiryTimeKey];
+        [[NSUserDefaults standardUserDefaults] setInteger:[self.passwordExpiryDate timeIntervalSince1970] forKey:kHITPADPassExpiryTimeKey];
     } else {
         NSInteger expirySince1970 = [[NSUserDefaults standardUserDefaults] integerForKey:kHITPADPassExpiryTimeKey];
-        self.passwordExpiracyDate = [NSDate dateWithTimeIntervalSince1970:expirySince1970];
+        self.passwordExpiryDate = [NSDate dateWithTimeIntervalSince1970:expirySince1970];
         self.lastADRequestSucceded = NO;
     }
+}
+
+- (void)sendUserNotificationWithDeliveryDate:(NSDate*)deliveryDate {
+    NSUserNotification *notification = [NSUserNotification new];
+    
+    notification.deliveryDate = deliveryDate;
+    notification.title = self.notificationTitle;
+    NSString *infoTextFormat = self.lastADRequestSucceded ? self.notificationMessageFormat : self.notificationOfflineMessageFormat;
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterMediumStyle];
+    [dateFormatter setTimeStyle:NSDateFormatterShortStyle];
+    
+    NSString *stringDate = [dateFormatter stringFromDate:self.passwordExpiryDate];
+    notification.informativeText = [NSString stringWithFormat:infoTextFormat, stringDate];
+    
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
 }
 
 @end
