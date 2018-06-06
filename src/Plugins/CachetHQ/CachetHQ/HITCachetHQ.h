52f35ded40a0ae418291fe33a2a4cb78683deafa
Yoann Gini <yoann.gini@gmail.com>
2018-06-06 20:05:36 +0200

    Cachet HQ introduction

---
+++ src/Plugins/CachetHQ/CachetHQ/HITCachetHQ.h
@@ -0,0 +1,15 @@
+//
+//  HITCachetHQ.h
+//  CachetHQ
+//
+//  Created by Yoann Gini on 05/06/2018.
+//  Copyright © 2018 Yoann Gini (Open Source Project). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <HITDevKit/HITDevKit.h>
+
+@interface HITCachetHQ : HITPeriodicPlugin
+
+@end
