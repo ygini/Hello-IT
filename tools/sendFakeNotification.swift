e72f4f9395d24b90ed3138567be21a4b2dcccaf5
Yoann Gini <yoann.gini@gmail.com>
2019-08-31 22:58:35 +0200

    Fix test tool to support new swift syntax

--- tools/sendFakeNotification.swift
+++ tools/sendFakeNotification.swift
@@ -4,6 +4,6 @@ import Foundation
 
 let userInfo = ["com.apple.MCX.changedDomains": ["com.github.ygini.Hello-IT"]]
 
-NSDistributedNotificationCenter.defaultCenter().postNotificationName("com.apple.MCX._managementStatusChangedForDomains", object: "com.apple.MCX", userInfo: userInfo) 
+DistributedNotificationCenter.default().post(name: Notification.Name("com.apple.MCX._managementStatusChangedForDomains"), object: "com.apple.MCX", userInfo: userInfo) 
 
 exit(0)
