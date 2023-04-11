53d66b601a08b3c8bf366cb2ec6f135db1086971
Yoann GINI <yoann.gini@gmail.com>
2023-04-11 15:31:08 +0200

    Support hideIfNotAvailable for resources under the file:// scheme fix #61

--- src/Plugins/OpenResource/OpenResource/HITPOpenResource.m
+++ src/Plugins/OpenResource/OpenResource/HITPOpenResource.m
@@ -11,9 +11,11 @@
 #import <Cocoa/Cocoa.h>
 #import <os/log.h>
 #define kHITPOpenResourceURL @"URL"
+#define kHITPOpenResourceHideIfNotAvailable @"hideIfNotAvailable"
 
 @interface HITPOpenResource ()
 @property NSURL *resource;
+@property BOOL hideIfNotAvailable;
 @end
 
 @implementation HITPOpenResource
@@ -23,9 +25,26 @@
     self = [super initWithSettings:settings];
     if (self) {
         _resource = [NSURL URLWithString:[settings objectForKey:kHITPOpenResourceURL]];
+        _hideIfNotAvailable = [[settings objectForKey:kHITPOpenResourceHideIfNotAvailable] boolValue];
+        
+        [self hideItemIfNeeded];
     }
     return self;
 }
+
+- (void)hideItemIfNeeded {
+    if (self.hideIfNotAvailable) {
+        if ([[self.resource scheme] isEqualToString:@"file"]) {
+            self.menuItem.hidden = ![[NSFileManager defaultManager] fileExistsAtPath:[self.resource path]];
+            if (self.menuItem.hidden) {
+                os_log_info(OS_LOG_DEFAULT, "Menu item %s for resource at path %s is hidden, path does not exist.", [self.menuItem.title cStringUsingEncoding:NSUTF8StringEncoding], [[self.resource path] cStringUsingEncoding:NSUTF8StringEncoding]);
+            }
+        }
+    } else {
+        self.menuItem.hidden = NO;
+    }
+}
+
 
 - (void)mainAction:(id)sender {
     os_log_info(OS_LOG_DEFAULT, "User requested to open %s", [[self.resource absoluteString] cStringUsingEncoding:NSUTF8StringEncoding]);
